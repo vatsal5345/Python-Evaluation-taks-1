5fe801be3719ad22518345b60faf9f7c201fbeea
vatsal5345 <vatsal5345@gmail.com>
2022-07-18 18:34:25 +0530

    change num=0 initial  value in main.py

--- evtask1/main.py
+++ evtask1/main.py
@@ -6,13 +6,13 @@ print("Welcome!! ")
 print(" 1) fibonaaci series")
 print(" 2) prime number")
 print(" 3) armstrong number")
-
-num = int(input("choose the problem : "))
-if num == 1:
+num = 0
+num2 = int(input("choose the problem : "))
+if num2 == 1:
     print(Fibonaaci_Series.run(num))
-elif num == 2:
+elif num2 == 2:
     print(Prime_Number.run(num))
-elif num == 3:
+elif num2 == 3:
     print(Armstrong_Number.run(num))
 else:
     print("""invalid entry!!!!
