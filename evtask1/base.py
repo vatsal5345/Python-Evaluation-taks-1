c90ce8e9de1d8a4c9b4189d4d7d09bedd1c37f9b
vatsal5345 <vatsal5345@gmail.com>
2022-07-18 17:13:59 +0530

    add task files

---
+++ evtask1/base.py
@@ -0,0 +1,6 @@
+from abc import ABC , abstractmethod
+
+class Abstract_class(ABC):
+    @abstractmethod
+    def run(self):
+        pass
---
+++ evtask1/problems/prime_number.py
@@ -0,0 +1,20 @@
+from base import Abstract_class
+
+class Prime_Number(Abstract_class):
+    def run(self):
+        """progam to fn the given number is prime or not"""
+        if num > 1:
+            for i in range(2, int((num/2)+1)):
+                if num % i == 0:
+                    print(num, "is not a prime number")
+                    break
+            else:
+                print(num, "is a prime number")
+                    
+
+        else:
+            print(num, "is not a prime number")
+        return ''
+    
+num = int(input("enter the number to check whether it is prime or not..  "))  
+print(Prime_Number.run(num)) 
